777dee15840ea733c1c41ae1d95d6b7476e3f645
RasmusThunborgPolestar <rasmus.thunborg@polestar.com>
2025-07-02 21:02:46 +0200

    Refactor project structure and update to .NET 9.0

---
+++ src/Infrastructure/RobotController.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DevoRobot
+{
+    public class RobotController(Robot robot)
+    {
+        private readonly Robot _robot = robot ?? throw new ArgumentNullException(nameof(robot));
+    }
+}
